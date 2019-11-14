21de1dd73b1eeee75396e7c3300154e95c2c8f0f
Venkata Manoj Reddy Dwarsala <vdwarsala@marketo.com>
2019-11-14 11:36:27 -0800

    Initial Maven Project

---
+++ Hackathon/src/main/java/com/marketo/hackathon/dataRequest.java
@@ -0,0 +1,5 @@
+package com.marketo.hackathon;
+
+public class dataRequest {
+
+}
---
+++ Hackathon/src/main/java/com/marketo/hackathon/textParse.java
@@ -0,0 +1,11 @@
+package com.marketo.hackathon;
+import edu.stanford.nlp.simple.*;
+
+public class textParse {
+
+    public static String text = "Lucy is in the sky with diamond. ";
+
+    public static void main(String[] args) {
+
+    }
+}
